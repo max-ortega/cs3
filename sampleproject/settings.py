43f3888eaa80bba47c2767980c17f023df52dee4
sorbetes6 <reuelgumawid6@gmail.com>
2024-12-12 17:51:32 +0800

    final3

--- sampleproject/settings.py
+++ sampleproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rg&u6znh)rxih$2i!n3dvh3$xl-pe=kp@jqqlf#y4+91$m8&#4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['reuelgumawid.pythonanywhere.com']
 
 
 # Application definition
